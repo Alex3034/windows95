356c463c289e6f6cb592a26bfbaa2e6437dce846
Alejandro <alex1348wk@gmail.com>
2025-01-10 00:29:06 +0100

    add WindowsMenu

---
+++ composeApp/src/desktopMain/kotlin/com/alex/windows95/components/windowsmenu/WindowsMenu.kt
@@ -0,0 +1,70 @@
+package com.alex.windows95.components.windowsmenu
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.alex.windows95.extensions.rotateVertically
+import com.alex.windows95.ui.backgroundComponent
+import com.alex.windows95.ui.windowsBarTextBackground
+import org.jetbrains.compose.resources.painterResource
+import windows95.composeapp.generated.resources.Res
+import windows95.composeapp.generated.resources.ic_documents
+import windows95.composeapp.generated.resources.ic_find
+import windows95.composeapp.generated.resources.ic_help
+import windows95.composeapp.generated.resources.ic_programs
+import windows95.composeapp.generated.resources.ic_run
+import windows95.composeapp.generated.resources.ic_settings
+import windows95.composeapp.generated.resources.ic_shutdown
+
+@Composable
+fun WindowsMenu() {
+    Column(
+        Modifier.height(350.dp)
+            .background(backgroundComponent)
+    ) {
+        Row {
+            Column {
+                Spacer(Modifier.weight(1f))
+                Box(
+                    modifier = Modifier.padding(start = 4.dp, top = 4.dp)
+                        .fillMaxHeight()
+                        .background(windowsBarTextBackground)
+                        .padding(4.dp),
+                    contentAlignment = Alignment.BottomStart
+                ) {
+                    Text(
+                        text = "Windows 95",
+                        modifier = Modifier.rotateVertically(false),
+                        fontSize = 42.sp
+                    )
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(200.dp)
+            ) {
+                WindowsMenuItem("Programs", painterResource(Res.drawable.ic_programs))
+                WindowsMenuItem("Documents", painterResource(Res.drawable.ic_documents))
+                WindowsMenuItem("Settings", painterResource(Res.drawable.ic_settings))
+                WindowsMenuItem("Find", painterResource(Res.drawable.ic_find))
+                WindowsMenuItem("Help", painterResource(Res.drawable.ic_help))
+                WindowsMenuItem("Run...", painterResource(Res.drawable.ic_run))
+                //DIVIDER
+                WindowsMenuItem("Shut Down...", painterResource(Res.drawable.ic_shutdown))
+            }
+        }
+    }
+}
