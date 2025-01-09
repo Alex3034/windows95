d3e7aabd56e56aa068d417dea66fc75e0c74a158
Alejandro <alex1348wk@gmail.com>
2025-01-09 01:07:46 +0100

    add Win button to WindowsBar

--- composeApp/src/desktopMain/kotlin/com/alex/windows95/components/WindowsBar.kt
+++ composeApp/src/desktopMain/kotlin/com/alex/windows95/components/WindowsBar.kt
@@ -1,20 +1,30 @@
 package com.alex.windows95.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.alex.windows95.ui.backgroundComponent
-import com.alex.windows95.ui.windowsBlue
+import org.jetbrains.compose.resources.painterResource
+import windows95.composeapp.generated.resources.Res
+import windows95.composeapp.generated.resources.winlogo
 
 @Composable
 fun WindowBar() {
@@ -29,15 +39,35 @@ fun WindowBar() {
             modifier = Modifier
                 .height(40.dp)
                 .fillMaxWidth()
-                .background(backgroundComponent)
+                .background(backgroundComponent),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Box(
+            WindowsButton(
                 Modifier
                     .padding(start = 8.dp)
-                    .fillMaxHeight()
-                    .width(80.dp)
-                    .background(windowsBlue)
-            )
+                    .height(36.dp)
+                    .width(95.dp),
+                onClick = {}
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        modifier = Modifier.size(36.dp),
+                        painter = painterResource(Res.drawable.winlogo),
+                        contentDescription = "Windows logo",
+                        contentScale = ContentScale.FillWidth
+                    )
+                    Spacer(Modifier.width(4.dp))
+                    Text(
+                        text = "Start",
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(bottom = 4.dp)
+                    )
+                }
+            }
             Row(modifier = Modifier.weight(1f)) { }
             InformationBar()
         }
