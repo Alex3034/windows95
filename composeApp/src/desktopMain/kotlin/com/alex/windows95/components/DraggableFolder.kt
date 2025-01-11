665e3de4e8e09f0b6f4bf45a395ae1bc55b7b4ab
Alejandro <alex1348wk@gmail.com>
2025-01-11 14:07:33 +0100

    add folder with move fun

---
+++ composeApp/src/desktopMain/kotlin/com/alex/windows95/components/DraggableFolder.kt
@@ -0,0 +1,54 @@
+package com.alex.windows95.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.vector.PathNode
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
+import com.alex.windows95.model.FolderModel
+import org.jetbrains.compose.resources.painterResource
+import windows95.composeapp.generated.resources.Res
+import windows95.composeapp.generated.resources.ic_folder
+
+@Composable
+fun DraggableFolder(folderModel: FolderModel, onMove: (Offset) -> Unit) {
+    var offset by remember { mutableStateOf(folderModel.position) }
+
+    Box(
+        Modifier
+            .offset(x = folderModel.position.x.dp, y = folderModel.position.y.dp)
+            .size(75.dp)
+            .pointerInput(Unit) {
+                detectTransformGestures { _, pan, _, _ ->
+                    offset = offset.copy(x = offset.x + pan.x, y = offset.y + pan.y)
+                    onMove(offset)
+                }
+            }
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                modifier = Modifier.weight(1f),
+                painter = painterResource(Res.drawable.ic_folder),
+                contentDescription = "folder"
+            )
+            Text(folderModel.name)
+        }
+    }
+}
--- composeApp/src/desktopMain/kotlin/com/alex/windows95/windows/Windows95Screen.kt
+++ composeApp/src/desktopMain/kotlin/com/alex/windows95/windows/Windows95Screen.kt
@@ -9,14 +9,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import com.alex.windows95.components.DraggableFolder
 import com.alex.windows95.components.WindowBar
 import com.alex.windows95.components.windowsmenu.WindowsMenuScreen
 import com.alex.windows95.extensions.clickableWithoutRipple
+import com.alex.windows95.model.FolderModel
 
 @Composable
 fun Windows95Screen() {
 
     var showWindowsMenu by remember { mutableStateOf(false) }
+    val fakeFolder = FolderModel(0, "Alejandro", Offset(x = 0f, y = 0f))
+    var folders by remember { mutableStateOf(listOf<FolderModel>(fakeFolder)) }
 
     Column {
         Box(
@@ -25,6 +30,13 @@ fun Windows95Screen() {
                 .weight(1f)
                 .clickableWithoutRipple { showWindowsMenu = false }
         ) {
+            folders.forEach { folder ->
+                DraggableFolder(folder, onMove = { newPosition ->
+                    folders = folders.map {
+                        if (it.id == folder.id) it.copy(position = newPosition) else it
+                    }
+                })
+            }
             WindowsMenuScreen(showWindowsMenu)
         }
         WindowBar { showWindowsMenu = !showWindowsMenu }
